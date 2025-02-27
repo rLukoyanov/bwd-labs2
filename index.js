97382f692f06e54cab7b5f90dfa98f9ba3c08a76
Ruslan Lukoyanov <ruslan.ru226@mail.ru>
2025-02-27 20:45:02 +0400

    Initial commit

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const cors = require("cors");
+const dotenv = require("dotenv");
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 5005;
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.json({
+    message: 'API is working'
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
